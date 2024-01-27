ac4a336dc317d4889ef9874abccc42f24fc2c491
catez.jeremy <catez.jeremy@hotmail.com>
2024-01-27 08:49:57 +0100

    feat(Database): Add Wynalia Database

--- src/main/java/fr/wynalia/citedestemples/Main.java
+++ src/main/java/fr/wynalia/citedestemples/Main.java
@@ -1,17 +1,48 @@
 package fr.wynalia.citedestemples;
 
+import fr.wynalia.database.classes.Database;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.sql.Connection;
+
 public final class Main extends JavaPlugin {
+    private static Main instance;
+    private FileConfiguration configuration;
+    private Database database;
+
+    public Main() {
+        saveDefaultConfig();
+        instance = this;
+    }
+
+    @Override
+    public void onLoad() {
+        configuration = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml"));
+        database = Database.getInstance(configuration.getString("db.url"), configuration.getString("db.user"), configuration.getString("db.password"));
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        database.connect();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        database.disconnect();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public FileConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public Connection getConnection() {
+        return database.getConnection();
     }
 }
