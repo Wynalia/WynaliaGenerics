fc57cd3c01d0d691d9942347aed458f8f905d1b5
Jeremy Catez <catez.jeremy@hotmail.com>
2024-03-05 13:39:10 +0100

    Init

--- src/main/java/fr/wynalia/citedestemples/Main.java
+++ src/main/java/fr/wynalia/citedestemples/Main.java
@@ -1,46 +1,24 @@
 package fr.wynalia.citedestemples;
 
-import fr.wynalia.citedestemples.teams.TeamManager;
-import fr.wynalia.database.classes.Database;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.sql.Connection;
-
 public final class Main extends JavaPlugin {
-    private static Main instance;
-    private static Database database;
 
     public Main() {
-        instance = this;
         saveDefaultConfig();
     }
 
     @Override
     public void onLoad() {
-        FileConfiguration config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml"));
-        database = Database.getInstance(config.getString("db.url"), config.getString("db.user"), config.getString("db.password"));
     }
 
     @Override
     public void onEnable() {
-        database.connect();
 
-        new TeamManager();
     }
 
     @Override
     public void onDisable() {
-        database.disconnect();
-    }
 
-    public static Main getInstance() {
-        return instance;
-    }
-
-    public static Connection getConnection() {
-        return database.getConnection();
     }
 }
