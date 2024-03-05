f3d41f09ad4ba06d9d5c9a36912efd44f1508927
Jeremy Catez <catez.jeremy@hotmail.com>
2024-03-05 11:03:11 +0100

    feat(teams): onJoin Player

---
+++ src/main/java/fr/dofsekai/generics/teams/GenericPlayerListener.java
@@ -0,0 +1,25 @@
+package fr.dofsekai.generics.teams;
+
+import fr.wynalia.templates.classes.Template;
+import fr.wynalia.templates.classes.TemplateReader;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+
+public class GenericPlayerListener implements Listener {
+    private final Plugin plugin;
+
+    public GenericPlayerListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Template template = TemplateReader.read(plugin.getDataFolder(), "config:chat.player-join");
+        template.set("username", player.getName());
+        event.setJoinMessage(template.render());
+    }
+}
---
+++ src/main/java/fr/dofsekai/generics/teams/GenericTeamManager.java
@@ -0,0 +1,15 @@
+package fr.dofsekai.generics.teams;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+public class GenericTeamManager {
+    public static void register(Plugin plugin) {
+        Bukkit.getPluginManager().registerEvents(new GenericPlayerListener(plugin), plugin);
+        GenericTeamManagement.getInstance(plugin);
+    }
+
+    public static void unregister(Plugin plugin) {
+        GenericTeamManagement.getInstance(plugin).getDatabase().disconnect();
+    }
+}
